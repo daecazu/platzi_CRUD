09ebb1a30f86be63129f7ee0f973d6354469e57c
Daniel Eduardo Cañón <daniel.canon@invytec.com>
2021-02-24 16:16:01 -0500

    clase: operaciones con strings y el comando update

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ def create_client(client_name):
     else:
         print('client is already in the client\'s list ')
 
+def update_client(client_name, updated_client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name+ ',')
+    else:
+        print('client not found')
+
 
 def list_clients():
     global clients
@@ -24,17 +31,26 @@ def _print_welcome():
     print('*' * 50)
     print('what would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate client')  
     print('[D]elete client')
+
+def _get_client_name(modificator=''):
+    return input(f"what's the {modificator+' '}client name? ")
 
 
 if __name__ == '__main__':
     _print_welcome()
-    command = input()
+    command = (input()).upper()
+
     if command == 'C':
-        client_name = input("what's the client name? ")
+        client_name = _get_client_name()
         create_client(client_name)
-        list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        update_client_name=_get_client_name('updated' )
+        update_client(client_name, update_client_name)
     else:
-        print('Invalid command') 
+        print('Invalid command')
+    list_clients()
